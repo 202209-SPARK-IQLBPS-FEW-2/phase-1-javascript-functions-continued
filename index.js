a5d4279561bf793dbc24e92cc679ed1c99bc01a6
roza <rozanawzad2001@gmail.com>
2022-12-07 15:08:03 +0300

    done js function continued  lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+   return  `This Saturday, I want to ${activity}!`
+     
+}
+saturdayFun();
+saturdayFun("bathe my dog")
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(result="*"){
+    const inner= function(emphatic="a hard worker"){
+        return `You are ${result}${emphatic}${result}!`
+    }
+    return inner
+}
+wrapAdjective("%")("a dedicated programmer")
